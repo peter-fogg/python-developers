d09c0ecc54ef5f2ed77da2331ca0907476ade74f
shiyingzheng <szheng@oberlin.edu>
2013-01-20 13:52:38 -0500

    Update picture/moveCircle.py

--- picture/moveCircle.py
+++ picture/moveCircle.py
@@ -1,22 +1,29 @@
 from tkinter import *
 
 class GUI(Frame):
-    def __init__(self, WIDTH, HEIGHT):
+    def __init__(self, param):
         Frame.__init__(self, None)
         self.grid()
 
-        MenuBar = Frame(self)
-        MenuBar.grid(row = 0, column = 0, sticky=W)
-        
-        QuitButton = Button(MenuBar, text="Quit", command = self.quit)
-        QuitButton.grid(row = 0, column = 0)
-        
         global canvas
-        canvas = Canvas(self, width=WIDTH, height=HEIGHT, background="white")
-        canvas.grid(row=1, column=0)
+        # Check if parameter is the right type, because we can't
+        # overload functions
+        if isinstance(param, tuple) and len(param) == 2:
+            canvas = Canvas(self, width=param[0], height=param[1], background="white")
+            canvas.grid()
+        elif isinstance(param, str):
+            self.image = PhotoImage(file=param)
+            canvas = Canvas(self, width=self.image.width(), height=self.image.height())
+            canvas.grid()
+            canvas.create_image(0, 0, image=self.image, anchor=NW)
+        else:
+            raise TypeError('Parameter to GUI() should be a string of a .gif/pgm/ppm file name or 2-tuple!')
 
         global penColor
-        penColor = "red"
+        penColor = "black"
+
+        global penWidth
+        penWidth = 1
 
     def drawCircle(self, x, y, r):
         circle = Circle(x, y, r)
@@ -55,14 +62,14 @@ def delay(millisecond):
 
 #Show circles at initial position for 2 secs, then move diagonally. 
 def main():
-    window = GUI(500, 500)
+    window = GUI((500, 500))
     circle1 = window.drawCircle(20, 20, 10)
     circle2 = window.drawCircle(480, 20, 10)
     display()
     delay(2000)
     for x in range(0, 470):
         moveby(circle1, 1, 1)
-        moveto(circle2, circle2.vertices[0][0]-1, circle2.vertices[0][1]+1)
+        moveto(circle2, circle2.getLocation()[0]-1, circle2.getLocation()[1]+1)
         display()
         delay(20)
     window.mainloop()

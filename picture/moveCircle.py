7c019c673b7a3656d017d0a98885fdf3cddd34c9
shiyingzheng <szheng@oberlin.edu>
2013-01-20 12:10:04 -0500

    Update picture/moveCircle.py

--- picture/moveCircle.py
+++ picture/moveCircle.py
@@ -1,38 +1,47 @@
 from tkinter import *
-from random import *
 
 class GUI(Frame):
     def __init__(self, WIDTH, HEIGHT):
         Frame.__init__(self, None)
         self.grid()
 
+        MenuBar = Frame(self)
+        MenuBar.grid(row = 0, column = 0, sticky=W)
+        
+        QuitButton = Button(MenuBar, text="Quit", command = self.quit)
+        QuitButton.grid(row = 0, column = 0)
+        
         global canvas
         canvas = Canvas(self, width=WIDTH, height=HEIGHT, background="white")
-        canvas.grid()
+        canvas.grid(row=1, column=0)
 
     def drawCircle(self, x, y, r):
-        self.shape = canvas.create_oval(x-r, y-r, x+r, y+r, fill = "black")
-        self.x = x
-        self.y = y
-        self.r = r
+        circle = canvas.create_oval(x-r, y-r, x+r, y+r, fill = "black")
+        return circle
 
-    def Animate(self):
-        self.move()
-        canvas.update()
-        canvas.after(1, self.Animate)
+def moveby(shape, dx, dy):
+    canvas.move(shape, dx, dy)
 
-    def move(self):
-        canvas.coords(self.shape, self.x - self.r + 1,
-                      self.y - self.r + 1,
-                      self.x + self.r + 1,
-                      self.y + self.r + 1)
-        self.x = self.x + 1
-        self.y = self.y + 1
+def moveto(shape, x1, y1, x2, y2):
+    canvas.coords(shape, x1, y1, x2, y2)
+
+def display():
+    canvas.update()
+
+def delay(millisecond):
+    canvas.after(millisecond)
 
 def main():
     window = GUI(500, 500)
-    window.drawCircle(20, 20, 10)
-    window.Animate()
+    circle1 = window.drawCircle(20, 20, 10)
+    circle2 = window.drawCircle(480, 20, 10)
+    display()
+    delay(2000)
+    for x in range(0, 470):
+        moveby(circle1, 1, 1)
+        moveby(circle2, -1, 1)
+        display()
+        delay(20)
     window.mainloop()
 
 main()

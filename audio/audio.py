fb8546ef31ad2662fe80ae2aacfebd7589edc05d
peter-fogg <peter.fogg@oberlin.edu>
2013-01-22 14:26:46 -0500

    Cleaned up/fixed audio.py -- in particular, we don't write a file to disk when it's not  necessary.

--- audio/audio.py
+++ audio/audio.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-import os
+import subprocess
+import sys
 import wave
 
 # All sorts of cross-platform support.
@@ -10,37 +11,42 @@ try:
     import winsound
     WINDOWS = True
 except ImportError:
-    try:
-        import ossaudiodev
-        LINUX = True
-    except ImportError:
+    if 'darwin' in sys.platform:
         OSX = True
+    elif 'linux' in sys.platform:
+        LINUX = True
 
-def write_file(sound, filename='audio.wav'):
+def get_bytes(sound):
     """
-    Takes an array of floats in the range [0, 1] and writes it to a
-    .wav file.
+    Converts a sequence of floats in the range [0, 1] to a bytestring
+    suitable for writing to a .wav file.
     """
-    w = wave.open(filename, 'w')
-    framerate = 44100
-    # (number of channels, samplewidth in bytes, framerate, number of frames, compression type, compression name)
-    w.setparams((1, 2, framerate, len(sound), 'NONE', 'noncompressed'))
     signal = b''
     for s in sound:
         signal += wave.struct.pack('h', int(1000*s))
+    return signal
+
+def write_file(sound, filename):
+    """Writes a sound to a .wav file."""
+    w = wave.open(filename, 'wb')
+    framerate = 44100
+    # (number of channels, samplewidth in bytes, framerate, number of frames, compression type, compression name)
+    w.setparams((1, 2, framerate, len(sound), 'NONE', 'noncompressed'))
+    signal = get_bytes(sound)
     w.writeframes(signal)
 
-def play(filename='audio.wav'):
+def play(sound, filename='.audio.wav'):
+    """Plays a sound, given as a sequence of floats in the range [0, 1]."""
     if WINDOWS:
-        winsound.PlaySound(sound, winsound.SND_FILENAME)
+        winsound.PlaySound(get_bytes(sound), winsound.SND_FILENAME)
     elif LINUX:
-        dev = ossaudiodev.open('w')
-        print('foo')
+        with subprocess.Popen('aplay', stdin=subprocess.PIPE) as process:
+            process.communicate(get_bytes(sound))
+            process.terminate()
     elif OSX:
-        os.system('afplay %s' % filename)
+        write_file(sound, filename)
+        subprocess.call(['afplay', filename])
 
 if __name__ == '__main__':
     import math
-    sound = [math.sin(2*math.pi*440*i/44100) for i in range(44100)]
-    write_file(sound)
-    play()
+    play([math.sin(2*math.pi*440*i/44100) for i in range(44100)])
